1e022a3fecaa409c96ce28fc50b7b40190e64f90
Vevek Selvam <vevek@macbook-pro.local>
2015-11-03 13:28:38 +0800

    Initial Commit

---
+++ Fevo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fevo
+//
+//  Created by Vevek Selvam on 3/11/15.
+//  Copyright © 2015 Vevek Selvam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
